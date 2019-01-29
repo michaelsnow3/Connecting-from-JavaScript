36bd107d01fb3a2cffd7d2275fe1432e82793a84
vagrant <mikesnow444@gmail.com>
2019-01-29 17:16:38 +0000

    added file that interacts with famous_people table. has function to lookup people in table by name

---
+++ lookup_people.js
@@ -0,0 +1,33 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+let name = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  selectByName(name);
+});
+
+function selectByName (inputName) {
+    client.query(`SELECT * FROM famous_people
+      WHERE first_name = $1 OR last_name = $1`, [inputName], (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      result.rows.forEach(function(row, i) {
+        console.log(`- ${i + 1}: ${row.first_name} ${row.last_name}, born '${row.birthdate.toISOString().split('T')[0]}'`);
+      });
+      client.end();
+  });
+}
